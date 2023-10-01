29425fe0ac3351add31517d2fdef57853e9e13af
Hassanali4 <89867379+Hassanali4@users.noreply.github.com>
2023-10-02 00:48:31 +0500

    Eve Unity Project
    i think its the first version of the eve project that i have made some changes that i have must of forgotten for now.

---
+++ Eve/Assets/Editor/CreateScriptTemplates.cs
@@ -0,0 +1,37 @@
+﻿//
+//Copyright (c) Hassan_Ali. All rights reserved 
+//
+
+using UnityEditor;
+
+public class CreateScriptTemplates
+{
+    [MenuItem("Asset/Create/Code/MonoBehaviour" , priority = 40)]
+    public static void CreateMonoBehaviourMenuItem()
+    {
+        string templatePath = "Resource/Assets/Editro/Templates/Monobehaviour.cs.txt";
+
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "NewScript.cs");
+    }
+    [MenuItem("Asset/Create/Code/Enum" , priority = 41)]
+    public static void CreateEnumMenuItem()
+    {
+        string templatePath = "Resource/Assets/Editro/Templates/Enum.cs.txt";
+
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "NewEnum.cs");
+    }
+    [MenuItem("Asset/Create/Code/ScrptableObjectM", priority = 42)]
+    public static void CreateScrptableObjectMenuItem()
+    {
+        string templatePath = "Resource/Assets/Editro/Templates/ScrptableObject.cs.txt";
+
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "NewScrptableObject.cs");
+    }
+    [MenuItem("Asset/Create/Code/Class", priority = 43)]
+    public static void CreateClassMenuItem()
+    {
+        string templatePath = "Resource/Assets/Editro/Templates/Class.cs.txt";
+
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "NewClass.cs");
+    }
+}
